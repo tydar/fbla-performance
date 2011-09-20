b7c3cb47672ba51bedac9739c90dcde917ace7de
Tyler Darnell <lokzor@gmail.com>
2011-09-20 13:10:34 -0400

    Still attempting to commit files fully--no major changes made, though the files should be more complete as of now.

---
+++ src/main/scala/fblatab/judge/model/judge.scala
@@ -0,0 +1,13 @@
+package fblatab.judge.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tydar
+ * Date: 9/16/11
+ * Time: 12:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class judge {
+
+}
